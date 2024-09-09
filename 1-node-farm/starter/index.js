741e26dcf1cc675a4afc1b6f0934ccce7e8bdce1
j27000 <namanjhala32@gmail.com>
2024-09-09 18:31:30 -0400

    09 sept

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,6 +1,23 @@
-import fs from 'fs';
-const  textin = fs.readFileSync('./txt/input.txt' ,'utf-8');
-console.log(textin);
-const write = " Hi i am naman jhala"
- fs.writeFileSync('./txt/output.txt', write);
- console.log("Written Operation SuccessFull");
+// import fs from 'fs';
+// const  textin = fs.readFileSync('./txt/input.txt' ,'utf-8');
+// console.log(textin);
+// const write = " Hi i am naman jhala"
+//  fs.writeFileSync('./txt/output.txt', write);
+//  console.log("Written Operation SuccessFull");
+
+import fs from 'fs'
+fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
+    if(err)
+    {
+        console.log("Error Occured");
+    }
+    console.log(data);
+    fs.readFile('./txt/read-this.txt', 'utf-8',(err,data2)=> {
+        console.log(data2);
+        fs.writeFile('./txt/naman.txt' ,`${data} and ${data2}`,(err)=>{
+            console.log("File Written");
+        })
+
+    })
+});
+console.log("hum first")
